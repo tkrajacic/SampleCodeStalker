f7b28643972e65c2fde6e7157b83bf6a6cd85d34
Thomas Krajacic <thomas@krajacic.at>
2016-01-23 08:27:09 +0100

    Initial Commit

---
+++ SampleCodeStalker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SampleCodeStalker
+//
+//  Created by Thomas Krajacic on 23.1.2016.
+//  Copyright © 2016 Thomas Krajacic. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
